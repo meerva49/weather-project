23f0d0afdc178f3dafd3f935f9cb710b8c74bd4a
Meera Vasishta <meerva49@gmail.com>
2026-07-07 00:48:02 -0700

    added weather data extraction for all 3 cities

--- python/extract.py
+++ python/extract.py
@@ -1,36 +1,49 @@
 import requests
 import json
 
-# open meteo historical weather API endpoint
-url = "https://archive-api.open-meteo.com/v1/archive"
-
-# parameters for API request
-params = {
-    # nyc coordinates
-    "latitude": 40.7128,
-    "longitude": -74.0060,
-    # data range for weather data
-    "start_date": "2024-01-01",
-    "end_date": "2024-01-31",
-    # daily weather metrics to retreive
-    "daily": "temperature_2m_max,precipitation_sum,wind_speed_10m_max"
+# dictionary having city names and coordinates
+cities = {
+    "new_york": (40.7128, -74.0060),
+    "phoenix": (33.4484, -112.0740),
+    "seattle": (47.6062, -122.3321)
 }
 
-# send GET request to API
-response = requests.get(url, params=params)
+# open-meteo historical weather API endpoint
+url = "https://archive-api.open-meteo.com/v1/archive"
 
-# print HTTP status code
-# 200 means request was successful
-print("Status Code:", response.status_code)
+# loop through each city
+for city, (lat, lon) in cities.items():
 
-# convert json response to Python dictionary
-data = response.json()
+    # parameters for API request
+    params = {
+        "latitude": lat,
+        "longitude": lon,
+        "start_date": "2024-01-01",
+        "end_date": "2024-12-31",
 
-# display top level keys by API
-#print(data.keys())
+        # daily weather metrics
+        "daily": (
+            "temperature_2m_max,"
+            "precipitation_sum,"
+            "wind_speed_10m_max"
+        )
+    }
 
-# convert json response into a python dictionary 
-with open("data/new_york_weather.json", "w") as file:
-    json.dump(data, file, indent=4)
+    # send GET request
+    response = requests.get(url, params=params)
 
-print("JSON file saved successfully.")
+    print(f"{city} Status Code:", response.status_code)
+
+    # convert JSON response into python dictionary
+    data = response.json()
+
+    # create file path
+    file_path = f"data/{city}.json"
+
+    # save JSON response
+    with open(file_path, "w") as file:
+        json.dump(data, file, indent=4)
+
+    print(f"{city}.json saved successfully.")
+
+print("\nAll city weather data has been downloaded.")
